4c0d483565b84ca538677934433af2bba865bafc
Braulio Alejo <braulioap2@hotmail.com>
2018-09-30 21:04:53 -0500

    Variables
    Primer ejemplo de variables

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables 
+{
+	public static void main(String[] args) 
+	{
+		byte edad = 20; //Declaración y asignación en una linea
+		
+		/*Imprime en la consola el
+		 * valor de la variable edad.
+		 */
+		System.out.println(edad);
+		edad = 25;
+		System.out.println(edad);
+	}
+}
